0eb152c06314fb09c1993cdc79e9eb7ac66b720e
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2021-03-30 14:11:14 +0100

    Delete CJEUprocessorDEMO/src/SDFTagger directory

--- CJEUprocessorDEMO/src/SDFTagger/KBInterface/KBInterface.java
+++
@@ -1,9 +0,0 @@
-package SDFTagger.KBInterface;
-
-import java.util.ArrayList;
-import SDFTagger.SDFItems.SDFHead;
-public interface KBInterface 
-{
-    public void fillBagsOfSDFNode(String Form, String Lemma, ArrayList<String> bagsOnForm, ArrayList<String> bagsOnLemma)throws Exception;
-    public ArrayList<String> retrieveSDFCodes(SDFHead SDFHead)throws Exception;
-}
